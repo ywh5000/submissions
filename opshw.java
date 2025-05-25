0375b70adbf40ed39a6815420cc5e13ce305a4b1
ywh5000 <yongwen102@outlook.com>
2025-05-25 20:06:57 +0100

    Add files via upload
    hw

---
+++ opshw.java
@@ -0,0 +1,9 @@
+public class opshw {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+        int z = x+y;
+        System.out.println(z);
+
+    }
+}
---
+++ yearcheck.java
@@ -0,0 +1,13 @@
+
+
+public class yearcheck {
+    public static void main(String[] args) {
+        int febdays = 29;
+        if (febdays == 28){
+            System.out.println("normal year");
+        }
+        if (febdays == 29){
+            System.out.println("leap year :)");
+        }
+    }
+}
